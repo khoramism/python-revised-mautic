902522a8a786180c723671ba9fbd69256a6bfee4
Alireza Khorami <khoramism@gmail.com>
2024-07-07 08:54:59 +0330

    Feat: Create and get emails

--- mautic/emails.py
+++ mautic/emails.py
@@ -35,3 +35,38 @@ class Emails(API):
             )
         )
         return self.process_response(response)
+    def create_email(self, name, subject, customHtml, **kwargs):
+        """
+        Create a new email
+        # Check here for more info: https://developer.mautic.org/?json#create-email
+        :param name: string
+        :param subject: string
+        :param customHtml: string
+        :param kwargs: other optional parameters for the email
+        :return: dict|str
+        """
+        payload = {
+            "name": name,
+            "subject": subject,
+            "customHtml": customHtml
+        }
+        payload.update(kwargs)
+
+        response = self._client.session.post(
+            '{url}/new'.format(url=self.endpoint_url), json=payload
+        )
+        return self.process_response(response)
+
+    def get_email(self, email_id):
+        """
+        Get an individual email by ID
+
+        :param email_id: int
+        :return: dict|str
+        """
+        response = self._client.session.get(
+            '{url}/{id}'.format(
+                url=self.endpoint_url, id=email_id
+            )
+        )
+        return self.process_response(response)
